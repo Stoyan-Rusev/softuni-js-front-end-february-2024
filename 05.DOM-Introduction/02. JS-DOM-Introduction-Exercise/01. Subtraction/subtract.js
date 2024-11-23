13e43fa3ad369089ab9e3add061f5073c08b0c13
Stoyan-Rusev <stoyan.rusevv@gmail.com>
2024-11-24 00:14:22 +0200

    Delete 05.DOM-Introduction/02. JS-DOM-Introduction-Exercise directory

--- 05.DOM-Introduction/02. JS-DOM-Introduction-Exercise/01. Subtraction/subtract.js
+++
@@ -1,9 +0,0 @@
-function subtract() {
-    const firstNum = Number(document.getElementById('firstNumber').value);
-    const secondtNum = Number(document.getElementById('secondNumber').value);
-    const resultNum = firstNum - secondtNum;
-
-    const resultElement = document.getElementById('result');
-    resultElement.textContent = resultNum;
-
-}
